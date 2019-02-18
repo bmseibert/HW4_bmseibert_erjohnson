f4e2a07342adcddcdde49f09fbac211a2a974f73
Your Name <you@example.com>
2019-02-18 15:49:59 -0500

    some doxy in Test2

--- src/Ant.cpp
+++ src/Ant.cpp
@@ -2,35 +2,63 @@
  * Ant.cpp
  *
  *  Created on: Feb 10, 2019
- *      Author: student
+ *      Author: Everett Johnson and Ben Seibert
  */
 
 #include "Ant.h"
 
-Ant::Ant()
-//:Organism(true)
+/** Ant::Ant() is the basic constructor for the
+ * ant class
+ *
+ */
+Ant::Ant() : Organism(true)
 {
 
 }
-Ant::Ant(int r, int c)
-:Organism(true)
+/** Ant::Ant() is another constructor for the
+ * ant class, placing the ant object at the location
+ * on the grid specified by the row and column
+ * parameters
+ * @param int r, the row where the ant will be constructed
+ * @param int c, the column where the ant will be constructed
+ * @param int life, the amount of ticks the ant object has
+ * been alive on the board
+ * @return
+ *
+ */
+Ant::Ant(int r, int c, int lifeCount) : Organism(true)
 {
 	row = r;
 	col = c;
+	lifeCount = 0;
 }
-
+/** Ant::move() should move an ant object to a neighboring
+ * cell
+ * @param
+ * @return status, true if the ant object moved
+ */
 bool Ant::move()
 {
 	bool status = true;
+
+
 	return status;
 }
-
+/** Ant::breed should check the cells around the ant
+ * object and if another ant in is a neighboring cell,
+ * then they will produce a new ant object in a free space
+ * @param
+ * @return status
+ */
 bool Ant::breed()
 {
 	bool status = true;
 	return status;
 }
-
+/** Ant::~Ant is a destructor for the ant class
+ * indicating that a ant object has died
+ *
+ */
 Ant::~Ant() {
 	// TODO Auto-generated destructor stub
 
